f4f2ae5d6cf266f7f21e6c31d182559d9582063a
lzt <1134565243@qq.com>
2022-05-01 13:40:19 +0800

    feat :删除多余代码

--- examples/App.vue
+++ examples/App.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="su">
-    <cd-image
-      src="https://cdn.jsdelivr.net/gh/lzt-T/img@master/Cee/carpediem.png"
-      :previewSrcList="[
-        'https://cdn.jsdelivr.net/gh/lzt-T/img@master/Cee/carpediem.png',
-      ]"
-      fit="cover"
-    ></cd-image>
-  </div>
-
-  <form method="post" action="http://127.0.0.1:3000/su">
-    <input type="submit" />
-  </form>
+  <div>欢迎</div>
 </template>
 <script lang="ts">
 import axios from "axios";
@@ -19,29 +7,10 @@ import { ref, onMounted, getCurrentInstance, watch, reactive } from "vue";
 export default {
   name: "App",
   setup() {
-    //   测试的数据
-    let isDrawer = ref<boolean>(false);
-    const changeIsDrawer = (data: boolean) => {
-      isDrawer.value = data;
-    };
-    const format = (percentage: number) =>
-      percentage === 100 ? "Full" : `${percentage}%`;
-    return {
-      changeIsDrawer,
-      format,
-      isDrawer,
-    };
+    return {};
   },
 };
 </script>
 
 <style >
-.su {
-  height: 200px;
-  width: 200px;
-}
-image {
-  height: 100%;
-  width: 100%;
-}
 </style>
